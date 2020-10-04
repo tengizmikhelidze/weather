d0e0f60c05902c948e720734c47fcc79af7e8c54
Tengiz Mikhelidze <mixelidzetengizi@gmail.com>
2020-10-05 01:49:45 +0400

    Updates

---
+++ precache-manifest.e7fc2502d421066ead4231b9f2e45c94.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64fbc0240a0020ef4ffc72d33b67c0d3",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "ab2d65519bb96761f5ed",
+    "url": "/weather/static/css/main.b0a3602c.chunk.css"
+  },
+  {
+    "revision": "7cf2182656a62ed6117a",
+    "url": "/weather/static/js/2.d437b892.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather/static/js/2.d437b892.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab2d65519bb96761f5ed",
+    "url": "/weather/static/js/main.0b9d787e.chunk.js"
+  },
+  {
+    "revision": "65a135568c88d392bf25",
+    "url": "/weather/static/js/runtime-main.d8725fa2.js"
+  },
+  {
+    "revision": "c97cb84b74a52edcb496295687e87822",
+    "url": "/weather/static/media/cold-bg.c97cb84b.jpg"
+  },
+  {
+    "revision": "635ed87d2cc826fb213057ac40dfaff1",
+    "url": "/weather/static/media/warm-bg.635ed87d.jpg"
+  }
+]);
